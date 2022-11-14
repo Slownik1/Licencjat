192b0e49e760b91af3d99559330b41bbd838bed2
dawid białek <dawid1bialek@gmail.com>
2022-11-14 21:50:31 +0100

    Add validation

--- src/main/java/com/example/Licencjat/outgoingModel.java
+++ src/main/java/com/example/Licencjat/outgoingModel.java
@@ -2,8 +2,14 @@ package com.example.Licencjat;
 
 import lombok.Data;
 import org.hibernate.annotations.GeneratorType;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import javax.validation.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "outgoing")
@@ -14,7 +20,12 @@ public class outgoingModel {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer Id;
 
-    private Double amount;
+    @Min(value = 0, message = "This value can not be less than 0")
+    private BigDecimal amount;
 
+    @NotBlank(message = "This value can not be blanc !")
+    private String user;
 
+    public outgoingModel() {
+    }
 }
--- src/main/java/com/example/Licencjat/outgoingsGroupModel.java
+++ src/main/java/com/example/Licencjat/outgoingsGroupModel.java
@@ -1,7 +1,19 @@
 package com.example.Licencjat;
 
-import org.hibernate.annotations.Entity;
+import lombok.Data;
+import lombok.NonNull;
+import org.hibernate.annotations.NotFound;
+import org.springframework.data.annotation.Id;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
 public class outgoingsGroupModel {
+
+    Long id;
+
+    String description;
+
 }
