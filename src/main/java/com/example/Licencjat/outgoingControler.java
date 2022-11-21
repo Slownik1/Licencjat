a4fb777a39d48d32a207239ccd9f3593c3e2a425
dawid białek <dawid1bialek@gmail.com>
2022-11-21 22:09:30 +0100

    Add sorting endpoint

---
+++ src/main/java/com/example/Licencjat/outgoingControler.java
@@ -0,0 +1,37 @@
+package com.example.Licencjat;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RepositoryRestController
+public class outgoingControler {
+
+    private final outgoingsRepository outgoingsRepository;
+
+    public outgoingControler(outgoingsRepository outgoingsRepository) {
+        this.outgoingsRepository = outgoingsRepository;
+    }
+
+    @GetMapping(value="/outgoingModels", params={"!sort", "!page", "!size"})
+    ResponseEntity<?> readAllOutgoings(){
+        return ResponseEntity.ok(outgoingsRepository.findAll());
+    }
+
+    @GetMapping("/outgoingModels")
+    ResponseEntity<?>readAllTaskAndSortByAmount(){
+        return ResponseEntity.ok(outgoingsRepository.findAll(Sort.by(Sort.Direction.ASC, "amount")));
+    }
+
+}
--- src/main/java/com/example/Licencjat/outgoingsRepository.java
+++ src/main/java/com/example/Licencjat/outgoingsRepository.java
@@ -1,12 +1,15 @@
 package com.example.Licencjat;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.Id;
 
 @RepositoryRestResource
-public interface outgoingsRepository extends JpaRepository<outgoingModel, Id> {
+interface outgoingsRepository extends JpaRepository<outgoingModel, Id> {
 
 
 
