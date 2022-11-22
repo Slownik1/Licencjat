10575f6fca9dc8125c291067f1a2694909c00ed2
dawid białek <dawid1bialek@gmail.com>
2022-11-22 22:11:14 +0100

    Add sorting endpoint + add page for sorting method

--- src/main/java/com/example/Licencjat/outgoingControler.java
+++ src/main/java/com/example/Licencjat/outgoingControler.java
@@ -4,12 +4,15 @@ package com.example.Licencjat;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,9 +32,9 @@ public class outgoingControler {
         return ResponseEntity.ok(outgoingsRepository.findAll());
     }
 
-    @GetMapping("/outgoingModels")
-    ResponseEntity<?>readAllTaskAndSortByAmount(){
-        return ResponseEntity.ok(outgoingsRepository.findAll(Sort.by(Sort.Direction.ASC, "amount")));
+    @GetMapping(value="/outgoingModels")
+    ResponseEntity<?>readAllTaskAndSortByAmount(Pageable page){
+        return ResponseEntity.ok(outgoingsRepository.findAll(page));
     }
 
 }
