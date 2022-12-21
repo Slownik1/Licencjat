66c5a078a84ef1e3c682b93a5941b3e83ed1cf50
Dawid Bialek <dawid.bialek@sekonixpoland.com>
2022-12-21 14:55:08 +0100

    Fix problems with img generation in templates

--- src/main/java/com/example/Licencjat/WebController.java
+++ src/main/java/com/example/Licencjat/WebController.java
@@ -8,9 +8,17 @@ public class WebController {
 
     @RequestMapping(value = "/index")
     public String Index(){
-
         return "index";
+    }
 
+    @RequestMapping(value = "/income")
+    public String Income(){
+        return "income";
+    }
+
+    @RequestMapping(value = "/outcome")
+    public String Outcome(){
+        return "outcome";
     }
 
 }
