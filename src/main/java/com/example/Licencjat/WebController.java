559f35d3e3cd68f3383f7f79c447b9febd4e80c6
Dawid Bialek <dawid.bialek@sekonixpoland.com>
2022-12-24 11:38:13 +0100

    Add error page

--- src/main/java/com/example/Licencjat/WebController.java
+++ src/main/java/com/example/Licencjat/WebController.java
@@ -1,22 +1,23 @@
 package com.example.Licencjat;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-    @RequestMapping(value = "/index")
+    @GetMapping(value = "/index")
     public String Index(){
         return "index";
     }
 
-    @RequestMapping(value = "/income")
+    @GetMapping(value = "/income")
     public String Income(){
         return "income";
     }
 
-    @RequestMapping(value = "/outcome")
+    @GetMapping(value = "/outcome")
     public String Outcome(){
         return "outcome";
     }
