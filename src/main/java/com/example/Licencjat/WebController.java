490d83e64ca006a266c3c2e7f6c2297a739a9468
Dawid Bialek <dawid.bialek@sekonixpoland.com>
2023-01-01 15:39:30 +0100

    Marge code

--- src/main/java/com/example/Licencjat/WebController.java
+++ src/main/java/com/example/Licencjat/WebController.java
@@ -1,24 +1,27 @@
 package com.example.Licencjat;
 
+import com.example.Licencjat.Outgoing.Outgoing;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 
-    @GetMapping(value = "/index")
+    @RequestMapping(value = "/index")
     public String Index(){
         return "index";
     }
 
-    @GetMapping(value = "/income")
+    @RequestMapping(value = "/income")
     public String Income(){
         return "income";
     }
 
     @GetMapping(value = "/outcome")
     public String Outcome(){
+        var project = new Outgoing();
         return "outcome";
     }
 
