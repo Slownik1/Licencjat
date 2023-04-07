d1186d7f92fa4efc2cb1a058db7919f3c4cc421d
DAWID-HP\dawid <dawid.bialek@sekonixpoland.com>
2023-04-07 13:40:35 +0200

    Add outgoing component

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutgoingComponent } from './outgoing/outgoing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutgoingComponent
   ],
   imports: [
     BrowserModule,
