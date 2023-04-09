a0179a540487fbe48fc28e02fde4bbd507ef2744
dawid białek <dawid1bialek@gmail.com>
2023-04-09 11:34:21 +0200

    add incoming component

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OutgoingComponent } from './outgoing/outgoing.component';
+import { IncomingComponent } from './incoming/incoming.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OutgoingComponent
+    OutgoingComponent,
+    IncomingComponent
   ],
   imports: [
     BrowserModule,
