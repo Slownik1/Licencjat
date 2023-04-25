73a5725c5daa66c9abe35e982c72b439332d7700
dawid białek <dawid1bialek@gmail.com>
2023-04-25 21:50:00 +0200

    Fix css for income and outcome form

--- angularclient/src/app/incoming/income.service.ts
+++ angularclient/src/app/incoming/income.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Income } from './income';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
