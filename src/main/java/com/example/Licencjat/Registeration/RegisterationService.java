a8ec0c241b07c4acd5087ff20f357d7db681b9de
dawid białek <dawid1bialek@gmail.com>
2023-05-08 11:47:59 +0200

    FIx problme with RegisterationService

--- src/main/java/com/example/Licencjat/Registeration/RegisterationService.java
+++ src/main/java/com/example/Licencjat/Registeration/RegisterationService.java
@@ -1,6 +1,8 @@
 package com.example.Licencjat.Registeration;
 
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegisterationService {
     public String register(RegisterationRequest request) {
         return "works";
