f2e055aa0d3e3fc2761909c75676cf29e2390f04
DAWID-HP\dawid <dawid.bialek@sekonixpoland.com>
2023-05-09 16:03:09 +0200

    Add confirmation endpoint

--- src/main/java/com/example/Licencjat/Registeration/RegisterationController.java
+++ src/main/java/com/example/Licencjat/Registeration/RegisterationController.java
@@ -1,10 +1,7 @@
 package com.example.Licencjat.Registeration;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/")
@@ -16,6 +13,11 @@ public class RegisterationController {
     @PostMapping
     public String register(@RequestBody RegisterationRequest request){
         return registerationService.register(request);
+    }
+
+    @GetMapping(path = "/confirmation")
+    public String confirm(@RequestParam("token") String token) {
+        return registerationService.confirmToken(token);
     }
 
 }
